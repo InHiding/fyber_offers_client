e4151d736fb921dd759b3d83ba3cd14cdf3f13bc
Giuliano Melo <giuliano.melo@abril.com.br>
2015-09-25 12:09:57 +0100

    more tests

--- spec/lib/fyber_client_spec.rb
+++ spec/lib/fyber_client_spec.rb
@@ -11,5 +11,16 @@ describe Fyber::Client do
       expect(resp.status).to be(400)
       expect(resp.body).to eq('{"code":"ERROR_INVALID_UID","message":"An invalid user id (uid) was given as a parameter in the request."}')
     end
+
+    it 'without timestamp' do
+      resp = fyber.get_offers(uid: 'user1')
+      expect(resp.status).to be(400)
+      expect(resp.body).to eq('{"code":"ERROR_INVALID_TIMESTAMP","message":"An invalid or expired timestamp was given as a parameter in the request."}')
+    end
+
+    it 'with timestamp' do
+      resp = fyber.get_offers(uid: 'user1', timestamp: '1443114790')
+      expect(resp.status).to be(200)
+    end
   end
 end
