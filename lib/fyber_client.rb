a11fb1554d122e36d46d2d5391449dc5e1d8da5e
Giuliano Melo <giuliano.melo@abril.com.br>
2015-09-25 12:18:31 +0100

    remove faraday logger

--- lib/fyber_client.rb
+++ lib/fyber_client.rb
@@ -7,7 +7,6 @@ module Fyber
 
       @conn = Faraday.new(url: 'http://api.fyber.com') do |faraday|
         faraday.request :json
-        faraday.response :logger #:json, content_type: /\bjson$/
         faraday.use :instrumentation
         faraday.adapter Faraday.default_adapter  # make requests with Net::HTTP
       end
